309f6a560dd742bb9f5e8e2eb886e51a8fbff394
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-09-25 01:01:04 +0600

    global filter and interceptor added

---
+++ src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface ApiResponse<T> {
+  success: true;
+  statusCode: number;
+  path: string;
+  data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, ApiResponse<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<ApiResponse<T>> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    return next.handle().pipe(
+      map((data) => ({
+        success: true as const,
+        statusCode: response.statusCode,
+        path: request.url,
+        data,
+      })),
+    );
+  }
+}
